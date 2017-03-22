f9ffac6c3719d79302de8d40d4d80e2cd6799cfc
Muthu kumar <muthu.k@omkarsoft.com>
2017-03-22 17:29:09 +0530

    second commit...

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('home1', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
